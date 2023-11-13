81eb78d79afd184eca8cdc4bce37abd3aeb77b4d
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-11-13 15:20:56 +0200

    CRUD Events

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT;
 const connection = require('./config/db');
+const eventsRouter = require('./routes/eventsRoute');
 
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use('/events', eventsRouter);
 
 
 app.listen(PORT, () => {
