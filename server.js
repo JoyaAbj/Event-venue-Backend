9f627fdca5e24606f084a3ab0ebf891670671d76
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-11-13 22:42:05 +0200

    done with venues

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const cors = require('cors');
 const PORT = process.env.PORT;
 const connection = require('./config/db');
 const eventsRouter = require('./routes/eventsRoute');
+const venuesRouter = require('./routes/venuesRoute');
 
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/events', eventsRouter);
+app.use('/venues', venuesRouter);
 
 
 app.listen(PORT, () => {
